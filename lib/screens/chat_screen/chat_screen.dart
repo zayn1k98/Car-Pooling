5ad4912ea498d51f5aeba296893e70245eab6539
unknown <syedar.lm10@gmail.com>
2023-12-10 14:49:11 +0530

    removed unused imports

--- lib/screens/chat_screen/chat_screen.dart
+++ lib/screens/chat_screen/chat_screen.dart
@@ -4,14 +4,12 @@ import 'dart:io';
 import 'package:car_pooling/models/message_model.dart';
 import 'package:car_pooling/screens/chat_screen/image_view.dart';
 import 'package:car_pooling/services/chat/chat_service.dart';
-import 'package:car_pooling/services/user/user_services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:intl/intl.dart';
 
